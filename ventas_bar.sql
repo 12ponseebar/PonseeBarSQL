331894d4834961120c2ecfe0fc15f05f405a727a
12ponseebar <ac7cheats@gmail.com>
2025-05-11 22:04:23 -0500

    Update licensias.json

--- ventas_bar.sql
+++ ventas_bar.sql
@@ -1,14 +1,129 @@
-CREATE TABLE IF NOT EXISTS productos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT NOT NULL,
-    precio REAL NOT NULL
-);
+{
 
-CREATE TABLE IF NOT EXISTS ventas (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    producto_id INTEGER,
-    cantidad INTEGER,
-    total REAL,
-    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (producto_id) REFERENCES productos (id)
-);
+    "[ZEUS]AC": { // ZEUS AC
+
+        /////////////////////////////////////////
+        /// PARA LA KEY (https://pinetools.com/random-string-generator) ///
+        /////////////////////////////////////////
+        "USER AQUI": { // PLANTILA LICENCIA 
+            "server_ip": "SERVER IP",
+            "user_key": "KEY AQUI", 
+        },
+        /////////////////////////////////////////
+
+        "TEST": { // Test Local Server User
+            "server_ip": "71.58.239.210:22003",
+            "user_key": "TEST",
+        },
+
+        "1291460444640247945": { // User Padre (Flow Latin)
+            "server_ip": "45.61.186.100:30000",
+            "user_key": "rM1NMXEHhWu7qbJwdqWBFwaDh2jKVLQz4DtiSnPk",
+        },
+
+        "699319342965325894": { // Usuarios jennix (DARK CITY RP) 
+            "server_ip": "102.129.137.216:20012",
+            "user_key": "4RnFn1hQd5J7wmLS4DYhUQ6g4zNFNHXtEX5P1AB7", 
+        },
+
+        "1069656224892719125": { // Usuario Roque (Asalto RP)
+            "server_ip": "192.99.241.164:22003",
+            "user_key": "vayEKrjdBXp6wm2kNprgquugWgefzpbU8cxSLuWk",
+        },
+
+        //  "850735540231471124": { // Usuarios NENE (Martin Roplay) 
+        //      "server_ip": "23.230.3.136:22025",
+        //     "user_key": "hzytmRuzTF02fqjYLYpVTNp1xnmm2vcQwH14Z1x2", 
+        //  },
+
+        "1243659117512491101": { // Usuarios NUNE (IMPERIO RP) 
+            "server_ip": "45.61.186.100:30001",
+            "user_key": "dqayi4nAC6cXznnkf49h20AdY2HQJfr7gkqbNPED", 
+        },
+
+        "1299254694002036749": { // Usuarios CHINO (One water ) 
+            "server_ip": "51.81.48.177:22027",
+            "user_key": "KmdeAn4Neg5tcWmg2TRTZSuYjMye41", 
+        },
+
+        "1227780335761100822": { // Usuarios RAYITO (CALENTON RP) 
+            "server_ip": "51.81.48.177:22016",
+            "user_key": "3czvV8PCMBSSLLxuBFV54STvSvJ71SDSb3byXP9H", 
+        },
+
+        "1108111123179376804": { // Usuarios Jarlin (Run RP) 
+            "server_ip": "144.217.16.122:22222",
+            "user_key": "DBPfX0RuxGKueEwAHeiVmeaxYPR7iZVfQJFFjMNh", 
+        },
+
+        "1186355965474390248": { // USER NEXO (LA RUMBA )
+            "server_ip": "51.81.48.177:22043",
+            "user_key": "fJqqpqYMPKAKu0bTj2p8Jy0i0wbX03", 
+        },
+
+        "FLAKO": { // FLAKO ( FLOW )
+            "server_ip": "45.61.186.100:30002",
+            "user_key": "KAKAKALDP",
+        },
+
+        "928857514110513253": { // ELWEIS ( La Capital Roleplay )
+            "server_ip": "147.135.31.37:22011",
+            "user_key": "40KtvvxmmEJwzrS9WyExd3AEFeRRXi70NeAyKA1N",
+        },
+
+
+        // "129146044464024794555": { // Usuario Mathia (Asfalto RP)
+        //     "server_ip": "51.81.48.177:22023",
+        //     "user_key": "rM1NMXEHhWu7qbJwdqWBFwaDh2jKVLQz4DtiSnPkkk",
+        // },
+        
+        // "1291460444640247945": { // Usuarios Juan (Naxes) 
+        //     "server_ip": "199.127.60.252:22000",
+        //     "user_key": "hzytmRuzTF02fqjYLYpVTNp1xnmm2vcQwH14Z1y4", 
+        // },
+
+    },
+    
+    "[ZEUS]PROTECTION": { // ZEUS SCRIPTS PROTECTION
+
+        /////////////////////////////////////////
+        /// PARA LA KEY (https://pinetools.com/random-string-generator) ///
+        /////////////////////////////////////////
+        "USER AQUI": { // PLANTILA LICENCIA 
+            "server_ip": "SERVER IP",
+            "user_key": "KEY AQUI", 
+        },
+        /////////////////////////////////////////
+
+        "TEST": { // Test Local Server User
+            "server_ip": "71.58.239.210:22003",
+            "user_key": "TEST",
+        },
+
+        "1291460444640247945": { // User Padre (Flow Latin)
+            "server_ip": "45.61.186.100:30000",
+            "user_key": "rM1NMXEHhWu7qbJwdqWBFwaDh2jKVLQz4DtiSnPk",
+        },
+
+        "1116025597806780416": { // Usuarios Flako (Flow New Jersey RP) 
+            "server_ip": "45.61.186.100:30002",
+            "user_key": "4RnFn1hQd5J7wmLS4DYhUQ6g4zNFNHXtEX5P1AB7", 
+        },
+
+        "1243659117512491101": { // USUARIOS NUÃ‘E (IMPERIO RP )
+        "server_ip": "45.61.186.100:30001",
+        "user_key": "dqayi4nAC6cXznnkf49h20AdY2HQJfr7gkqbNPED",
+    },
+
+      "928857514110513253": { // user // elweis // La capital  
+            "server_ip": "147.135.31.37:22011",
+            "user_key": "TMkRMjeQiqZAfqj34X34kLnjdug3ZZ", 
+        },
+
+        // "850735540231471124": { // Usuarios NENE (Martin Roplay) 
+        //     "server_ip": "23.230.3.136:22025",
+        //     "user_key": "hzytmRuzTF02fqjYLYpVTNp1xnmm2vcQwH14Z1x2", 
+        // },
+
+    },
+}
